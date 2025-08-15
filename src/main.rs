528a6d0d200c0085f017e52418ce148aef67bc6a
Jen Farwell <jenfarwell21@gmail.com>
2025-08-15 14:05:05 -0700

    reorganized functions
    Signed-off-by: Jen Farwell <jenfarwell21@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
 fn main() {
 
-    let foo = is_prime(7903);
+    let n = 7901;
+    let foo = is_prime(n);
 
     if foo {
-        println!("foo is prime");
+        println!("{n} is prime");
     } else {
-        println!("foo is not prime");
+        println!("{n} is not prime");
+    }
+
+    let n = 7727;
+    let foo = is_prime_wheel(n);
+
+    if foo {
+        println!("{n} is prime");
+    } else {
+        println!("{n} is not prime");
     }
 }
 
 fn is_prime(n: u32) -> bool {
 
+    match n {
+        0 => return false, // zero is not prime
+        1 => return false, // one is also not prime
+        2 => return true, // two is the first prime
+        _ => {},
+    }
+
+
+    // // all even numbers except 2 are not prime
+    if n % 2 == 0 {
+        println!("even number, bailed early");
+        return false;
+    } else {
+        // compute up to sqrt(n) rounded down, from 3.
+        // should we go down from sqrt(n) instead?
+        let sqrt_n = n.isqrt();
+
+        // we only need to test odd numbers in the search space,
+        // so we can cut our time in half per number by adjusting
+        // the step of the iterator.
+        for i in (3..sqrt_n).step_by(2) {
+            if n % i == 0 {
+                println!("searched up to {i}");
+                return false;
+            }
+        }
+        println!("PRIME: searched up to {sqrt_n}");
+        return true;
+    }
+}
+
+fn is_prime_wheel(n: u32) -> bool {
     match n {
         0 => return false, // zero is not prime
         1 => return false, // one is also not prime
@@ -35,26 +77,4 @@ fn is_prime(n: u32) -> bool {
         }
         return true;
     }
-
-    // // all even numbers except 2 are not prime
-    // if n % 2 == 0 {
-    //     println!("even number, bailed early");
-    //     return false;
-    // } else {
-    //     // compute up to sqrt(n) rounded down, from 3.
-    //     // should we go down from sqrt(n) instead?
-    //     let sqrt_n = n.isqrt();
-
-    //     // we only need to test odd numbers in the search space,
-    //     // so we can cut our time in half per number by adjusting
-    //     // the step of the iterator.
-    //     for i in (3..sqrt_n).step_by(2) {
-    //         if n % i == 0 {
-    //             println!("searched up to {i}");
-    //             return false;
-    //         }
-    //     }
-    //     println!("PRIME: searched up to {sqrt_n}");
-    //     return true;
-    // }
 }
