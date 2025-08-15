cb4efdc65b0d1b7397ff7bb18d841a752128fb43
Jen Farwell <jenfarwell21@gmail.com>
2025-08-15 11:43:23 -0700

    First iteration of is_prime()
    Implementing a primality test function for my own learning purposes. I
    am giving myself a computer science education.
    
    This basic implementation works. We are performance optimizing here in a
    couple different ways:
    
    1. if n is an even number we bail out right away
    2. count up from the bottom of the range to sqrt(n)
    
    This cuts time complexity to O(sqrt(n)) but it's actually better than
    that because half the number space is early bailout before we start
    looping. There may be other tests we can do to bail out early.
    
    Going to continue to play with this and implement further optimizations and
    concurrent processing of the number space.
    
    Signed-off-by: Jen Farwell <jenfarwell21@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+
+    let foo = is_prime(7867);
+
+    if foo {
+        println!("foo is prime");
+    } else {
+        println!("foo is not prime");
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    match n {
+        0 => return false, // zero is not prime
+        1 => return false, // one is also not prime
+        2 => return true, // two is the first prime and is always prime
+        _ => {},
+    }
+
+    // all even numbers except 2 are not prime
+    if n % 2 == 0 {
+        return false;
+    } else {
+        // compute up to sqrt(n) rounded down
+        let sqrt_n = n.isqrt();
+
+        for i in 3..sqrt_n {
+            if n % i == 0 {
+                return false;
+            }
+        }
+        return true;
+    }
+}
